43bef16017dde4582590b44e779e84c5c8d2d471
JP Mohr <James.Mohr2.0@hotmail.com>
2024-05-01 15:35:29 -0400

    fixed var statement and interpolation

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -14,10 +14,10 @@
            
         
                 
-            int answer = 4;
+            var answer = 4;
             string response = (answer < 9) ?
-            response = answer + " is less than nine" :
-            answer + " is greater than or equal to nine";
+            response = ($"{ answer} is less than nine") :
+            ($"{answer} is greater than or equal to nine");
             Console.WriteLine(response);
             }
     }
