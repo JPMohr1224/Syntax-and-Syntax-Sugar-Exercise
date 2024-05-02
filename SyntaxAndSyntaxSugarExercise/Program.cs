04b25f7babbe6fd865c4d3831542de1970726bb2
JP Mohr <James.Mohr2.0@hotmail.com>
2024-05-02 16:40:38 -0400

    corrected ternary expression

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -15,8 +15,8 @@
         
                 
             var answer = 4;
-            string response = (answer < 9) ?
-            response = ($"{ answer} is less than nine") :
+            var response = (answer < 9) ?
+            ($"{ answer} is less than nine") :
             ($"{answer} is greater than or equal to nine");
             Console.WriteLine(response);
             }
